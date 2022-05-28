7d7ab234fe9face167f4857ddc6147625140c9a3
LentoAssai <88371913+LentoAssai@users.noreply.github.com>
2022-05-28 22:41:14 +0900

    May 28

--- Algorithm/Programmers/Level1/main.swift
+++ Algorithm/Programmers/Level1/main.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+func solution(_ n: Int, _ lost: [Int], _ reserve: [Int]) -> Int {
+    var students: [Int] = []
+    for i in 0..<n {
+        students.append(1)
+        
+        if lost.contains(i+1) {
+            students[i] -= 1
+        }
+        if reserve.contains(i+1) {
+            students[i] += 1
+        }
+    }
+    
+    for i in 0..<n {
+        if i > 0 && students[i] == 2 && students[i-1] == 0 {
+            students[i] -= 1
+            students[i-1] += 1
+        }
+        if i < n-1 && students[i] == 2 && students[i+1] == 0 {
+            students[i] -= 1
+            students[i+1] += 1
+        }
+    }
+    
+    return students.filter { $0 > 0 }.count
+}
